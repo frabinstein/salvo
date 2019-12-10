3399d02868440c1cd4cb2ece10ddde460c7ccf56
frabinstein <frabinstein@users.noreply.github.com>
2019-12-09 22:26:32 -0300

    Added functionality to create new games

--- src/main/resources/static/web/scripts/game.js
+++ src/main/resources/static/web/scripts/game.js
@@ -5,6 +5,7 @@ var gamePlayerData = "#gamePlayerData";
 var shipData = "#shipData";
 var salvoData = "#salvoData";
 
+var gameStatus = "";
 var activePlayer = {};
 var opponent = {};
 var ships = [];
@@ -24,98 +25,110 @@ function queryStringParameter(queryString) {
 };
 
 function main(json) {
-    activePlayer = json.gamePlayers.filter(gamePlayer => gamePlayer.id === parseInt(gamePlayerId))[0].player;
+  gameStatus = json.status;
+  activePlayer = json.gamePlayers.filter(gamePlayer => gamePlayer.id === parseInt(gamePlayerId))[0].player;
+  if(gameStatus != "open") {
     opponent = json.gamePlayers.filter(gamePlayer => gamePlayer.id !== parseInt(gamePlayerId))[0].player;
     ships = json.ships;
     playerSalvoShots = json.salvoes.filter(playerSalvoes => playerSalvoes.player === activePlayer.email)[0];
     opponentSalvoShots = json.salvoes.filter(playerSalvoes => playerSalvoes.player === opponent.email)[0];
+  }
 
-    $(gamePlayerData).append(buildGamePlayerDataHTML());
+  $(gamePlayerData).append(buildGamePlayerDataHTML());
+  if(gameStatus != "open") {
     $(shipData).append(buildShipsIntroHTML());
     $(salvoData).append(buildSalvoesIntroHTML());
-    $(shipData).append(buildBaseHTMLGrid("ships"));
-    $(salvoData).append(buildBaseHTMLGrid("salvoes"));
+  }
+  $(shipData).append(buildBaseHTMLGrid("ships"));
+  $(salvoData).append(buildBaseHTMLGrid("salvoes"));
 
+  if(gameStatus != "open") {
     populateShips("ships");
     populateSalvoes(opponentSalvoShots, "ships");
     populateSalvoes(playerSalvoShots, "salvoes");
+  }
 }
 
 
 function buildGamePlayerDataHTML() {
-    let content = "";
-    content += "Hello " + activePlayer.name + ",<br><br>";
+  let content = "";
+  content += "Hello " + activePlayer.name + ",<br><br>";
+  if(gameStatus == "open") {
+    content += "You are waiting for an opponent to join this game<br><br>";
+  }
+  else {
     content += "You are playing this game against " + opponent.name + "<br><br>";
-    return content;
+  }
+  return content;
 };
 
 function buildShipsIntroHTML() {
-    let content = "";
-    content += "These are your ships:";
-    content += "<ol>" + ships.map(ship => "<li>" + ship.type + " (" + ship.shipLocations + ")</li>").join("") + "</ol>";
-    return content;
+  let content = "";
+  content += "These are your ships:";
+  content += "<ol>" + ships.map(ship => "<li>" + ship.type + " (" + ship.shipLocations + ")</li>").join("") + "</ol>";
+  return content;
 };
 
 function buildSalvoesIntroHTML() {
-    let content = "";
-    content += "These are the salvoes you have shot:";
-    content += "<ul>";
-    content += playerSalvoShots.salvoes.map(salvo => "<li>Turn " + salvo.turn + ": " + salvo.salvoLocations + "</li>").join("") + "</ul>";
-    return content;
+  let content = "";
+  content += "These are the salvoes you have shot:";
+  content += "<ul>";
+  content += playerSalvoShots.salvoes.map(salvo => "<li>Turn " + salvo.turn + ": " + salvo.salvoLocations + "</li>").join("") + "</ul>";
+  return content;
 };
 
 function buildBaseHTMLGrid(tableClass) {
-    let content = "";
+  let content = "";
 
-    //Open table:
-    content += "<table border=1 class='" + tableClass + "'><colgroup><col class='table_header'></colgroup>";
+  //Open table:
+  content += "<table border=1 class='" + tableClass + "'><colgroup><col class='table_header'></colgroup>";
 
-    //Build header row:
-    content += "<thead class='table_header'><tr>";
-    for(let i = 0; i < 11; i++) {
-        if(i === 0)
-            content += "<th></th>";
-        else
-            content += "<th>" + (i) + "</th>";
+  //Build header row:
+  content += "<thead class='table_header'><tr>";
+  for(let i = 0; i < 11; i++) {
+    if(i === 0)
+      content += "<th></th>";
+    else
+      content += "<th>" + (i) + "</th>";
+  };
+  content += "</tr></thead>";
+
+  //Build table body:
+  content += "<tbody>";
+  for(let i = 0; i < 10; i++) {
+    content += "<tr>";
+    for(let j = 0; j < 11; j++) {
+      let row = String.fromCharCode('A'.charCodeAt(0)+i);
+      let cell = row.concat(j);
+      if(j === 0)
+        content += "<th>" + row + "</th>";
+      else {
+        content += "<td id='" + tableClass + cell + "'></td>";
+      }
     };
-    content += "</tr></thead>";
+    content += "</tr>";
+  };
+  content += "</tbody>";
 
-    //Build table body:
-    content += "<tbody>";
-    for(let i = 0; i < 10; i++) {
-        content += "<tr>";
-        for(let j = 0; j < 11; j++) {
-            let row = String.fromCharCode('A'.charCodeAt(0)+i);
-            let cell = row.concat(j);
-            if(j === 0)
-                content += "<th>" + row + "</th>";
-            else {
-                content += "<td id='" + tableClass + cell + "'></td>";
-            }
-        };
-        content += "</tr>";
-    };
-    content += "</tbody>";
+  //Close table:
+  content += "</table>";
 
-    //Close table:
-    content += "</table>";
-
-    return content;
+  return content;
 };
 
 
 function populateShips(tableClass){
-    ships.forEach(ship => ship.shipLocations.forEach(cell => $("#"+tableClass+cell).addClass("occupied")));
+  ships.forEach(ship => ship.shipLocations.forEach(cell => $("#"+tableClass+cell).addClass("occupied")));
 
 };
 
 function populateSalvoes(salvoShots, tableClass){
-    salvoShots.salvoes.forEach(salvo => salvo.salvoLocations.forEach(cell => {
-        $("#"+tableClass+cell).addClass("shot");
-        $("#"+tableClass+cell).append(salvo.turn);
-        }));
+  salvoShots.salvoes.forEach(salvo => salvo.salvoLocations.forEach(cell => {
+    $("#"+tableClass+cell).addClass("shot");
+    $("#"+tableClass+cell).append(salvo.turn);
+    }));
 };
 
 
 $.getJSON(uri, json => main(json))
-    .fail( response => console.log("Request Failed: " + response.responseJSON.error) );
+  .fail( response => console.log("Request Failed: " + response.responseJSON.error) );
--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -13,200 +13,200 @@ var finished = "#finished";
 
 
 function main(json) {
-    json.forEach( game => {
-        switch(game.status) {
-            case "open":
-                openGames.push(game);
-                break;
-            case "not started":
-            case "ongoing":
-                ongoingGames.push(game);
-                break;
-            case "finished":
-                finishedGames.push(game);
-                break;
-        };
-    });
+  json.forEach( game => {
+    switch(game.status) {
+      case "open":
+        openGames.push(game);
+        break;
+      case "not started":
+      case "ongoing":
+        ongoingGames.push(game);
+        break;
+      case "finished":
+        finishedGames.push(game);
+        break;
+    };
+  });
 
-    $(open).html(buildGamesTable("Open", openGames));
-    $(ongoing).html(buildGamesTable("Ongoing", ongoingGames));
-    $(finished).html(buildGamesTable("Finished", finishedGames));
+  $(open).html(buildGamesTable("Open", openGames));
+  $(ongoing).html(buildGamesTable("Ongoing", ongoingGames));
+  $(finished).html(buildGamesTable("Finished", finishedGames));
 
-    getAuthentication();
+  getAuthentication();
 };
 
 
 function buildGamesTable(heading, games) {
-    let content = "<thead><tr><th colspan='3'>" + heading + " games</th></tr></thead>";
-    content += "<tbody>";
-    content += games.map(game => buildGameRow(game));
-    content += "</tbody>";
-    return content;
+  let content = "<thead><tr><th colspan='3'>" + heading + " games</th></tr></thead>";
+  content += "<tbody>";
+  content += games.map(game => buildGameRow(game));
+  content += "</tbody>";
+  return content;
 }
 
 function buildGameRow(game) {
-    let content = "";
-    content += "<tr><td class='col1'>Game #" + game.id + "</td><td class='col2'>";
-    switch(game.status) {
-        case "open":
-            content += "<b>" + game.gamePlayers[0].player.name + "</b> is waiting for an opponent";
-            content += "</td><td class='col3'><button type='button' class='join' hidden>Join game</button></td></tr>";
-            break;
-        case "not started":
-        case "ongoing":
-            content += "<b>" + game.gamePlayers.map(gamePlayer => gamePlayer.player.name).join("</b> vs. <b>") + "</b>";
-            content += "</td><td class='col3'>"
-                + game.gamePlayers.map(gamePlayer =>
-                    "<button type='button' class='return' player='"
-                    + gamePlayer.player.id
-                    + "' onclick='redirectTo(\"/web/game.html?gp="
-                    + gamePlayer.id
-                    + "\")' hidden>Return to your game</button>")
-                    .join("")
-                + "</td></tr>";
-            break;
-        case "finished":
-            content += "<b>" + game.gamePlayers.map(gamePlayer => gamePlayer.player.name).join("</b> vs. <b>") + "</b>";
-            content += "<b>" + game.result + "</b>";
-            content += "</td><td class='col3'></td></tr>";
-            break;
-    };
-    return content;
+  let content = "";
+  content += "<tr><td class='col1'>Game #" + game.id + "</td><td class='col2'>";
+  switch(game.status) {
+    case "open":
+      content += "<b>" + game.gamePlayers[0].player.name + "</b> is waiting for an opponent";
+      content += "</td><td class='col3'><button type='button' class='join' hidden>Join game</button></td></tr>";
+      break;
+    case "not started":
+    case "ongoing":
+      content += "<b>" + game.gamePlayers.map(gamePlayer => gamePlayer.player.name).join("</b> vs. <b>") + "</b>";
+      content += "</td><td class='col3'>"
+        + game.gamePlayers.map(gamePlayer =>
+          "<button type='button' class='return' player='"
+          + gamePlayer.player.id
+          + "' onclick='redirectTo(\"/web/game.html?gp="
+          + gamePlayer.id
+          + "\")' hidden>Return to your game</button>")
+          .join("")
+        + "</td></tr>";
+      break;
+    case "finished":
+      content += "<b>" + game.gamePlayers.map(gamePlayer => gamePlayer.player.name).join("</b> vs. <b>") + "</b>";
+      content += "<b>" + game.result + "</b>";
+      content += "</td><td class='col3'></td></tr>";
+      break;
+  };
+  return content;
 }
 
 
 function getAuthentication() {
-    $.getJSON(authentication, json => authenticatedUser = json)
-        .done( () => {
-            buildHeader();
-            if(authenticatedUser.id != 0)
-                showButtons();
-        })
-        .fail( response => console.log("Request Failed: " + response.responseJSON.error) );
+  $.getJSON(authentication, json => authenticatedUser = json)
+    .done( () => {
+      buildHeader();
+      if(authenticatedUser.id != 0)
+        showButtons();
+    })
+    .fail( response => console.log("Request Failed: " + response.responseJSON.error) );
 }
 
 function buildHeader() {
-    let content = "";
-    if( authenticatedUser.id == 0 ) {
-        content += "<p>Welcome! Please login or sign up:</p>";
+  let content = "";
+  if( authenticatedUser.id == 0 ) {
+    content += "<p>Welcome! Please login or sign up:</p>";
 
-        content += "<div class='header'>";
-        content += "<form name='login-form' onsubmit='return false'>";
-        content += "<div class='warning'></div>";
-        content += "<label>E-mail: <input type='text' name='email'></label>";
-        content += "<label>Password: <input type='password' name='password'></label>";
-        content += "<input type='submit' value='Log in' id='login'>";
-        content += "</form>";
+    content += "<div class='header'>";
+    content += "<form name='login-form' onsubmit='return false'>";
+    content += "<div class='warning'></div>";
+    content += "<label>E-mail: <input type='text' name='email'></label>";
+    content += "<label>Password: <input type='password' name='password'></label>";
+    content += "<input type='submit' value='Log in' id='login'>";
+    content += "</form>";
 
-        content += "<form name='signup-form' onsubmit='return false'>";
-        content += "<div class='warning'></div>";
-        content += "<label>Name: <input type='text' name='name'></label>";
-        content += "<label>E-mail: <input type='text' name='email'></label>";
-        content += "<label>Password: <input type='password' name='password'></label>";
-        content += "<label>Retype password: <input type='password' name='password2'></label>";
-        content += "<input type='submit' value='Sign up' id='signup'>";
-        content += "</form>";
-        content += "</div>";
-    }
-    else {
-        content += "<p>Hello " + authenticatedUser.name + "!</p>";
+    content += "<form name='signup-form' onsubmit='return false'>";
+    content += "<div class='warning'></div>";
+    content += "<label>Name: <input type='text' name='name'></label>";
+    content += "<label>E-mail: <input type='text' name='email'></label>";
+    content += "<label>Password: <input type='password' name='password'></label>";
+    content += "<label>Retype password: <input type='password' name='password2'></label>";
+    content += "<input type='submit' value='Sign up' id='signup'>";
+    content += "</form>";
+    content += "</div>";
+  }
+  else {
+    content += "<p>Hello " + authenticatedUser.name + "!</p>";
 
-        content += "<div class='header'>";
-        content += "<button type='button' id='logout'>Log out</button>";
-        content += "<button type='button' id='create'>Create game</button>";
-        content += "</div>";
-    }
-    $("header").html(content);
+    content += "<div class='header'>";
+    content += "<button type='button' id='logout'>Log out</button>";
+    content += "<button type='button' id='create'>Create game</button>";
+    content += "</div>";
+  }
+  $("header").html(content);
 }
 
 function showButtons() {
-    $('#open .col3 .join').removeAttr("hidden");
-    $('#ongoing .col3 .return[player="'+authenticatedUser.id+'"]').removeAttr("hidden");
+  $('#open .col3 .join').removeAttr("hidden");
+  $('#ongoing .col3 .return[player="'+authenticatedUser.id+'"]').removeAttr("hidden");
 }
 
 
 function trimField(form, field) {
-    $('[name="'+form+'"] [name="'+field+'"]').val($('[name="'+form+'"] [name="'+field+'"]').val().trim());
+  $('[name="'+form+'"] [name="'+field+'"]').val($('[name="'+form+'"] [name="'+field+'"]').val().trim());
 
 }
 
 function validateForm(form) {
-    if(form == "signup-form")
-        if(!validateField(form, "name"))
-            return false;
-    if(!validateField(form, "email"))
-        return false;
-    if(!validateField(form, "password"))
-        return false;
-    if(form == "signup-form") {
-        if(($('[name="'+form+'"] [name="password"]').val().localeCompare($('[name="'+form+'"] [name="password2"]').val())) !== 0) {
-            alert("Passwords don't match");
-            return false;
-        }
+  if(form == "signup-form")
+    if(!validateField(form, "name"))
+      return false;
+  if(!validateField(form, "email"))
+    return false;
+  if(!validateField(form, "password"))
+    return false;
+  if(form == "signup-form") {
+    if(($('[name="'+form+'"] [name="password"]').val().localeCompare($('[name="'+form+'"] [name="password2"]').val())) !== 0) {
+      alert("Passwords don't match");
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 function validateField(form, field) {
-    if($('[name="'+form+'"] [name="'+field+'"]').val() == "") {
-        alert(field + " can't be blank");
-        return false;
-    }
-    if((field == "email" || field == "password") && $('[name="'+form+'"] [name="'+field+'"]').val().indexOf(" ") != -1) {
-        alert(field + " can't contain blank spaces");
-        return false;
-    }
-    if(field == "email" && $('[name="'+form+'"] [name="'+field+'"]').val().indexOf("@") == -1) {
-        alert("incorrect " + field + " format");
-        return false;
-    }
-    return true;
+  if($('[name="'+form+'"] [name="'+field+'"]').val() == "") {
+    alert(field + " can't be blank");
+    return false;
+  }
+  if((field == "email" || field == "password") && $('[name="'+form+'"] [name="'+field+'"]').val().indexOf(" ") != -1) {
+    alert(field + " can't contain blank spaces");
+    return false;
+  }
+  if(field == "email" && $('[name="'+form+'"] [name="'+field+'"]').val().indexOf("@") == -1) {
+    alert("incorrect " + field + " format");
+    return false;
+  }
+  return true;
 }
 
 
 function login(email, password) {
-    $.post("/api/login", { "email": email, "password": password }, () => getAuthentication(""))
-        .fail( () => $('[name="login-form"] .warning').html("<p><span class='error'>Login error!</span> Please check your credentials</p>") );
+  $.post("/api/login", { "email": email, "password": password }, () => getAuthentication(""))
+    .fail( () => $('[name="login-form"] .warning').html("<p><span class='error'>Login error!</span> Please check your credentials</p>") );
 }
 
 function logout() {
-    $.post("/api/logout", () => {
-        getAuthentication("You have logged out successfully");
-        redirectTo("/web/games.html");
-    })
-        .fail( () => $("header").prepend("<p class='error'>Logout error!</p>") );
+  $.post("/api/logout", () => {
+    getAuthentication("You have logged out successfully");
+    redirectTo("/web/games.html");
+  })
+    .fail( () => $("header").prepend("<p class='error'>Logout error!</p>") );
 }
 
 function signup(name, email, password) {
-    $.post("/api/players", { "name": name, "email": email, "password": password }, () => login(email, password) )
-        .fail( response => $('[name="signup-form"] .warning').html("<p><span class='error'>There was an error:</span> " + response.responseJSON.error + ". Please try again.</p>") );
+  $.post("/api/players", { "name": name, "email": email, "password": password }, () => login(email, password) )
+    .fail( response => $('[name="signup-form"] .warning').html("<p><span class='error'>There was an error:</span> " + response.responseJSON.error + ". Please try again.</p>") );
 }
 
 function createGame() {
-    $.post("/api/games", json => redirectTo("/web/game.html?gp=" + json.gamePlayer_id) )
-        .fail( response => alert("Request Failed: " + response.responseJSON.error) );
+  $.post("/api/games", json => redirectTo("/web/game.html?gp=" + json.gamePlayer_id) )
+    .fail( response => alert("Request Failed: " + response.responseJSON.error) );
 }
 
 
 function redirectTo(target)   {
-    location.href = target;
+  location.href = target;
 
 }
 
 
 $.getJSON(uri, json => main(json))
-    .fail( response => console.log("Request Failed: " + response.responseJSON.error) );
+  .fail( response => console.log("Request Failed: " + response.responseJSON.error) );
 
 $('header').on('click', '#login', "login-form", event => {
-    trimField(event.data, "email");
-    if(validateForm(event.data))
-        login($('[name="'+event.data+'"] [name="email"]').val(), $('[name="'+event.data+'"] [name="password"]').val())
+  trimField(event.data, "email");
+  if(validateForm(event.data))
+    login($('[name="'+event.data+'"] [name="email"]').val(), $('[name="'+event.data+'"] [name="password"]').val())
 });
 
 $('header').on('click', '#signup', "signup-form", event => {
-    trimField(event.data, "email");
-    if(validateForm(event.data))
-        signup($('[name="'+event.data+'"] [name="name"]').val(), $('[name="'+event.data+'"] [name="email"]').val(), $('[name="'+event.data+'"] [name="password"]').val())
+  trimField(event.data, "email");
+  if(validateForm(event.data))
+    signup($('[name="'+event.data+'"] [name="name"]').val(), $('[name="'+event.data+'"] [name="email"]').val(), $('[name="'+event.data+'"] [name="password"]').val())
 });
 
 $('header').on('click', '#logout', () => logout() );
